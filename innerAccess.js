2f0e79d042fcc210baac49fa0710e831d13949d4
Julia <yb259@cam.ac.uk>
2017-08-07 20:59:15 +0200

    accessing inner HTML

---
+++ innerAccess.js
@@ -0,0 +1,7 @@
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+document.getElementById("demo2").innerHTML = "x + y = " + z;
+document.getElementById("demo2").style.fontSize = "30px";
+document.getElementById("demo2").style.color = "#0000FF";
