1ba90d6ef44bfc8d4791acbc2ad475d459656ab3
Julia <yb259@cam.ac.uk>
2017-08-10 22:25:27 +0200

    Variables, objects, types

---
+++ variables.js
@@ -0,0 +1,13 @@
+var whatisit;
+var welcome = "Welcome" + ", " + "Julia"
+document.getElementById("undef").innerHTML = whatisit;
+document.getElementById("undef").style.color = "red";
+document.getElementById("str").innerHTML = welcome;
+document.getElementById("str").style.color = "green";
+var x = 2 + 3 + "5"
+var y = "3" + 2 + 3 
+document.getElementById("concat1").innerHTML = x;
+document.getElementById("concat2").innerHTML = y;
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+document.getElementById("obj").innerHTML = "Name is: " + person.firstName + "<br>" + "Surname is: " + person.lastName + "<br>" + "Age is: " + person.age;
+document.getElementById("typ").innerHTML = "Types are: " + typeof(5) + " " + typeof(" ") + " " + typeof(und) + " " + typeof(true) + " " + null ;
