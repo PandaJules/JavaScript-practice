a6c8a6c0611a74ab5103af8f3780b3ace14fb951
Julia <yb259@cam.ac.uk>
2017-08-11 22:39:52 +0200

    String comparison and access

--- variables.js
+++ variables.js
@@ -8,6 +8,12 @@ var x = 2 + 3 + "5"
 var y = "3" + 2 + 3 
 document.getElementById("concat1").innerHTML = x;
 document.getElementById("concat2").innerHTML = y;
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-document.getElementById("obj").innerHTML = "Name is: " + person.firstName + "<br>" + "Surname is: " + person.lastName + "<br>" + "Age is: " + person.age;
+var person = {
+    firstName: "Julia",
+    lastName : "Roberts",
+    id       :  1355,
+    age		 :  50,
+    fullName : function() {return this.firstName + " " + this.lastName;}
+};
+document.getElementById("obj").innerHTML = "Name is: " + person.firstName + "<br>" + "Surname is: " + person["lastName"] + "<br>" + "Age is: " + person.age;
 document.getElementById("typ").innerHTML = "Types are: " + typeof(5) + " " + typeof(" ") + " " + typeof(und) + " " + typeof(true) + " " + null ;
