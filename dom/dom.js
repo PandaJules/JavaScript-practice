d1bb3cf13bdcb4f298259626f4febb83b0e2aac4
Julia <yb259@cam.ac.uk>
2017-08-30 22:41:39 +0200

    animations and mouse events

--- dom/dom.js
+++ dom/dom.js
@@ -3,4 +3,41 @@ document.getElementById("second").innerHTML = "The second paragraph is:<br>" + x
 document.getElementById("total").innerHTML = "There are " + x.length + " paragraphs, including this one";
 
 var y = document.querySelectorAll("p.middle");
-document.getElementById("query").innerHTML = 'The first paragraph with class="middle": ' + x[0].innerHTML;
+document.getElementById("query").innerHTML = 'The first paragraph with class="middle": ' + y[0].innerHTML;
+
+
+function slide() {
+  var elem = document.getElementById("block");  
+  var cont = document.getElementById('container') 
+  var top_pos = 0;
+  var left_pos = 0
+  var id = setInterval(frame, 1);
+  function frame() {
+	block_width = elem.offsetWidth;
+	block_height = elem.offsetHeight;
+	container_width = cont.offsetWidth;
+	container_height = cont.offsetHeight;
+    if (left_pos >= container_width-block_width) {
+      clearInterval(id);
+    } else {
+      top_pos += 1; 
+      left_pos += (container_width-block_width)/(container_height-block_height);
+      elem.style.top = top_pos + 'px'; 
+      elem.style.left = left_pos + 'px'; 
+    }
+  }
+}
+
+function mOver(obj) {obj.innerHTML = "No, get off me now!"}
+
+function mOut(obj) {obj.innerHTML = "Thank you :)"}
+
+function mDown(obj) {
+    obj.style.backgroundColor = "red";
+    obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+    obj.style.backgroundColor="#009933";
+    obj.innerHTML="Clicked!";
+}
--- styleChange.js
+++
@@ -1,5 +0,0 @@
-function makeRed() {
-    document.getElementById('demo3').innerHTML="You've changed it!";
-    document.getElementById('demo3').style.color="red";
-
-}
