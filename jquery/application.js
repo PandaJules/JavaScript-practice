5842f88fcbf67b413659dbd20aae7dc91f47b8f4
PandaJules <bibik.julia.96@gmail.com>
2017-09-05 19:03:43 +0200

    Refactored button interaction and removed code duplication

--- jquery/application.js
+++ jquery/application.js
@@ -4,13 +4,25 @@ jQuery(document).ready(function(){
 	var last = $(".tasks > li").last()[0].childNodes[0].nodeValue;
 	var second = $(".tasks > li").first().next()[0].childNodes[0].nodeValue;
 	var second_last = $(".tasks > li").last().prev()[0].childNodes[0].nodeValue;
-	document.getElementById("traverse").innerHTML = `First element: \"${first}\"<br>\
-													Last: \"${last}\"<br>\
-													One after first: \"${second}\"<br>\
-													Second last: \"${second_last}\"`;
-	$('button').on('click', function() {
+	$("#traverse").append($("<p id='first'>"+`First element: \"${first}\"`+"</p>"));
+	$("#traverse").append($("<p id='last'>"+`Last: \"${last}\"`+"</p>"));
+	$("#traverse").append($("<p>"+`One after first: \"${second}\"`+"</p>"));
+	$("#traverse").append($("<p>"+`Second last: \"${second_last}\"`+"</p>"));
+
+	$('#todo').on('click', 'button', function() {
 		var task = $('<li>Wash the dishes</li>');
    		$('.tasks').append(task);
-   		$('button').remove();
+   		var last = $(".tasks > li").last()[0].childNodes[0].nodeValue;
+   		$("#traverse").find("#last").text(`Last: \"${last}\"`);
+   		$(this).remove();
    	});
+
+
+   	$('.tour').on('click', 'button', function() {
+    var tour = $(this).closest('.tour');
+    var discount = tour.data('discount');
+    var message = $('<span>Call 1-555-jquery-air for a $' + discount + ' discount.</span>');
+    tour.append(message);
+    $(this).remove();
+  });
 });
