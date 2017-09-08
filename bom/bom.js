aa0085f40678a5d24dd923a0d13887f002880196
PandaJules <bibik.julia.96@gmail.com>
2017-09-08 14:40:37 +0200

    Pop up boxes

--- bom/bom.js
+++ bom/bom.js
@@ -6,3 +6,24 @@ document.getElementById("loc").innerHTML +=  "<br><b>location:</b>  " + window.l
 function google() {
     window.location.assign("https://www.google.com")
 }
+
+function confirmMe() {
+    var txt;
+    if (confirm("Choose a button") == true) {
+        txt = "I know what you pressed! You pressed <span style='color:red'>OK</span>!";
+    } else {
+        txt = "I know what you pressed! You pressed <span style='color:blue'>Cancel!</span>";
+    }
+    document.getElementById("confirm").innerHTML = txt;
+}
+
+function greetMe() {
+    var txt;
+    var person = prompt("Please enter your name:", "John Doe");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello <span style='color:red'>" + person + "</span>! Welcome to this website. Are you doing okay?";
+    }
+    document.getElementById("greet").innerHTML = txt;
+}
